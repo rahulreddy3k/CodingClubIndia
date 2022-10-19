98d3dd3e5e4f3db6d8f1e7776afc0237937cd5fa
Rahul <67961235+rahulreddy3k@users.noreply.github.com>
2022-10-19 19:59:55 +0530

    Update HelloWorld.java

--- Hello_World_in_every_language/HelloWorld.java
+++ Hello_World_in_every_language/HelloWorld.java
@@ -3,5 +3,6 @@ import java.util.*;
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("WOW, I have started to contibuted to Open Source");
     }
 }
